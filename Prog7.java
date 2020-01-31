916cb847b34a3e87b47423fedb11bc4bc8ce42fe
Isabel <59679787+Isabelppz@users.noreply.github.com>
2020-01-30 16:40:03 -0800

    Add files via upload

---
+++ Prog7.java
@@ -0,0 +1,22 @@
+public class Prog7{
+    public static void main(final String args[]){
+        int num1=3;
+        int num2=5;
+        int num3=7;
+        int num4=6;
+        int num5=3;
+        int num6=2;
+        int num7=9;
+        System.out.println("The sum of all values is:"+(num1+num2+num3+num4+num5+num6+num7));
+
+        final int [] numbers={3,5,7,6,3,2,9};
+        int num;
+            for(int i=0; i<numbers.length;i++){
+            num=numbers[i];
+             System.out.println("number: "+ numbers[i]);
+            } 
+        
+            if(numbers.equals(7));{
+            System.out.println("Hi Everybody");}         
+    }
+}
